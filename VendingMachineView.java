4cd8819563d50bb66fefdf64dc17a04c7a89825e
Jawsshhh <137859762+Jawsshhh@users.noreply.github.com>
2023-07-31 23:43:43 +0800

    Update VendingMachineView.java

--- VendingMachineView.java
+++ VendingMachineView.java
@@ -1,8 +1,8 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-//import javax.swing.JTextField;
+import javax.swing.JTextField;
 import javax.swing.JButton;
-//import javax.swing.JTextArea;
+import javax.swing.JTextArea;
 import javax.swing.JPanel;
 
 import java.awt.Container;
@@ -14,9 +14,9 @@ import java.awt.event.ActionListener;
 
 public class VendingMachineView{
     private JFrame mainFrame;
-	private JLabel idLbl, nameLbl, feedbackLbl;
-	//private JTextField idTf, nameTf;
-	private JButton RegVend, SpecVend;
+	private JLabel nameLbl, Items, ilName, ilPrice, ilQuantity, ilCalories, ilCategory;
+	private JTextField iName, iPrice, iQuantity, iCalories, iCategory;
+	private JButton RegVend, SpecVend, itemSubmit;
 	//private JTextArea employeeListTextArea;
 
     public VendingMachineView(){
@@ -39,13 +39,108 @@ public class VendingMachineView{
         this.mainFrame.add(panel);
         this.mainFrame.add(RegVend);
         this.mainFrame.add(SpecVend);
-        this.mainFrame.pack();
+        
 
+        //CREATE ITEMS PART
+        this.Items = new JLabel("Create Items");
+        this.ilName = new JLabel("Name: ");
+        this.ilPrice = new JLabel("Price: ");
+        this.ilQuantity = new JLabel("Quantity: ");
+        this.ilCalories = new JLabel("Calories: ");
+        this.ilCategory = new JLabel("Category: ");
+        this.mainFrame.add(Items);
+        this.mainFrame.add(ilName);
+        
+        this.Items.setVisible(false);
+        this.ilName.setVisible(false);
+        this.ilPrice.setVisible(false);
+        this.ilQuantity.setVisible(false);
+        this.ilCalories.setVisible(false);
+        this.ilCategory.setVisible(false);
+        
+        this.iName = new JTextField();
+		this.iName.setColumns(10);
+        this.iPrice = new JTextField();
+		this.iPrice.setColumns(10);
+        this.iQuantity = new JTextField();
+		this.iQuantity.setColumns(10);
+        this.iCalories = new JTextField();
+		this.iCalories.setColumns(10);
+        this.iCategory = new JTextField();
+		this.iCategory.setColumns(10);
+
+        this.mainFrame.add(iName);
+        this.mainFrame.add(ilPrice);
+        this.mainFrame.add(iPrice);
+        this.mainFrame.add(ilQuantity);
+        this.mainFrame.add(iQuantity);
+        this.mainFrame.add(ilCalories);
+        this.mainFrame.add(iCalories);
+        this.mainFrame.add(ilCategory);
+        this.mainFrame.add(iCategory);
+
+        this.iName.setVisible(false);
+        this.iName.setEnabled(false);
+        this.iPrice.setVisible(false);
+        this.iPrice.setEnabled(false);
+        this.iQuantity.setVisible(false);
+        this.iQuantity.setEnabled(false);
+        this.iCalories.setVisible(false);
+        this.iCalories.setEnabled(false);
+        this.iCategory.setVisible(false);
+        this.iCategory.setEnabled(false);
+
+        itemSubmit = new JButton("Submit");
+        itemSubmit.setBounds(10, 10, 60, 60);
+        this.itemSubmit.setEnabled(false);
+        this.itemSubmit.setVisible(false);
+        this.mainFrame.add(itemSubmit);
+        //create "Add Item" button
+        // VENDING MENUS
+    
+        this.mainFrame.pack();
+        
     }
     public void setAddBtnListener(ActionListener actionListener) {
 		this.RegVend.addActionListener(actionListener);
+        this.SpecVend.addActionListener(actionListener);
+        
 	}
+    public void itemSubmitListener(ActionListener actionListener) {
+		this.itemSubmit.addActionListener(actionListener);//change itemSubmit to AddItem button
+        
+        
+	}
+    //create actionlistener for submitting items; only available if >=8 items
 
+    public void FirstMenuStatus (boolean status){
+        this.RegVend.setEnabled(status);
+        this.RegVend.setVisible(status);
+        this.SpecVend.setEnabled(status);
+        this.SpecVend.setVisible(status);
+        this.nameLbl.setVisible(status);
+    }
+
+    public void ItemCreate(boolean status){
+        this.Items.setVisible(status);
+        this.ilName.setVisible(status);
+        this.ilPrice.setVisible(status);
+        this.ilQuantity.setVisible(status);
+        this.ilCalories.setVisible(status);
+        this.ilCategory.setVisible(status);
+        this.iName.setVisible(status);
+        this.iName.setEnabled(status);
+        this.iPrice.setVisible(status);
+        this.iPrice.setEnabled(status);
+        this.iQuantity.setVisible(status);
+        this.iQuantity.setEnabled(status);
+        this.iCalories.setVisible(status);
+        this.iCalories.setEnabled(status);
+        this.iCategory.setVisible(status);
+        this.iCategory.setEnabled(status);
+        this.itemSubmit.setEnabled(status);
+        this.itemSubmit.setVisible(status);
+    }
     public String getLabel(){
         return this.nameLbl;
     }
@@ -56,13 +151,37 @@ public class VendingMachineView{
 	public void setViewBtnListener(ActionListener actionListener) {
 		this.viewBtn.addActionListener(actionListener);
 	}
+    public String getNameTF(){
+        return this.ilName.getText();
+    }
+    public int getPriceTF(){
+        return this.ilPrice.getText();
+    }
+    public int getQuantityTF(){
+        return this.ilQuantity.getText();
+    }
+    public float getCaloriesTF(){
+        return this.ilCalories.getText();
+    }
+    public String getCategoryTF(){
+        return this.ilCategory.getText();
+    }
+    public void clearItemTF (){
+        this.ilName.setText("");
+        this.ilPrice.setText("");
+        this.ilQuantity.setText("");
+        this.ilCalories.setText("");
+        this.ilCategory.setText("");
+    }
 
-
+    
 
 public static void main (String[] args) {
+    
     VendingMachineView newMachine = new VendingMachineView();
 
     VendingMachineController vController = new VendingMachineController(newMachine);
+
 }
 
 }
