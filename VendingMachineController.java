8ba88e12a6a8987a2b4514a174e63fffd3d1be87
Jawsshhh <137859762+Jawsshhh@users.noreply.github.com>
2023-07-31 23:44:10 +0800

    Update VendingMachineController.java

--- VendingMachineController.java
+++ VendingMachineController.java
@@ -12,10 +12,30 @@ public class VendingMachineController {
     this.VendingView.setAddBtnListener(new ActionListener(){
         @Override
         public void actionPerformed (ActionEvent e){
-           VendingView.setLabel("Changed");
+           VendingView.FirstMenuStatus(false);
+           VendingView.ItemCreate(true);
         }
 
 
     });
+
+    this.VendingView.itemSubmitListener(new ActionListener() {
+        @Override
+        public void actionPerformed (ActionEvent a){
+            String name = VendingView.getNameTF();
+            int price = VendingView.getPriceTF();
+            int quantity = VendingView.getQuantityTF();
+            float calories = VendingView.getCaloriesTF();
+            String category = VendingView.getCategoryTF();
+
+            
+            //make temporary class with Jlabels and pass parameters to test if data stores
+            // call vending model for addItems(paramters) called by Add Item button
+            VendingView.clearItemTF();
+            
+            VendingView.ItemCreate(false); // only available if >= 8  items
+        }
+    });
     }
 }
+
