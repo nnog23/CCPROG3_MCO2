015cbc1a116e128f5fb5f67c5f82c95d372f962e
nnog23 <136414069+nnog23@users.noreply.github.com>
2023-08-01 15:26:55 +0800

    Update VendingMachineController.java

--- VendingMachineController.java
+++ VendingMachineController.java
@@ -4,38 +4,112 @@ import java.awt.event.ActionListener;
 public class VendingMachineController {
 
     private VendingMachineView VendingView;
-    private int x = 0;
-    public VendingMachineController (VendingMachineView VendingView){
-        this.VendingView = VendingView;
-    
+    private VendingMachineModel VendingModel;
+    private boolean isVendingMachineCreated = false;
 
-    this.VendingView.setAddBtnListener(new ActionListener(){
+    private int x = 0;
+    public VendingMachineController (VendingMachineView VendingView, VendingMachineModel VendingModel){
+
+        this.VendingView = VendingView;
+        this.VendingModel = VendingModel;
+
+    this.VendingView.setRegVendBtnListener(new ActionListener(){
         @Override
         public void actionPerformed (ActionEvent e){
+
+          
+        if(!isVendingMachineCreated){
+
            VendingView.FirstMenuStatus(false);
-           VendingView.ItemCreate(true);
+           VendingView.RegularItemCreate(true);
+           
+        }else{
+
+
+
+        }
+           
+
         }
 
 
     });
 
-    this.VendingView.itemSubmitListener(new ActionListener() {
+    this.VendingView.setSpecVendBtnListener(new ActionListener(){
+        @Override
+        public void actionPerformed (ActionEvent e){
+
+
+        if(!isVendingMachineCreated){
+           
+           VendingView.FirstMenuStatus(false);
+           VendingView.SpecialItemCreate(true);
+
+        }else{
+
+            
+        }
+
+        }
+
+
+    });
+
+     this.VendingView.setRegSubmitListener(new ActionListener() {
         @Override
         public void actionPerformed (ActionEvent a){
+
+            // do while VendingMachineModel.getItemList().size() < 8
+
+            VendingView.Done(false);
+
+            String name = VendingView.getNameTF();
+            int price = VendingView.getPriceTF();
+            int quantity = VendingView.getQuantityTF();
+            float calories = VendingView.getCaloriesTF();
+
+
+            
+            //make temporary class with Jlabels and pass parameters to test if data stores
+            // call vending model for addItems(paramters) called by Add Item button
+
+            VendingView.clearItemTF();
+
+            VendingModel.addRegItem(name, price, quantity, calories);
+           
+            if(VendingModel.getItemList().size() >= 8){
+
+            VendingView.Done(true); // only available if >= 8  items
+
+            }
+
+        }
+    });
+    
+
+    this.VendingView.setSpecSubmitListener(new ActionListener() {
+        @Override
+        public void actionPerformed (ActionEvent a){
+
             String name = VendingView.getNameTF();
             int price = VendingView.getPriceTF();
             int quantity = VendingView.getQuantityTF();
             float calories = VendingView.getCaloriesTF();
             String category = VendingView.getCategoryTF();
 
+
             
             //make temporary class with Jlabels and pass parameters to test if data stores
             // call vending model for addItems(paramters) called by Add Item button
             VendingView.clearItemTF();
             
-            VendingView.ItemCreate(false); // only available if >= 8  items
+            VendingView.SpecSubmit(false); // only available if >= 8  items
+
         }
     });
+
+
+
     }
 }
 
