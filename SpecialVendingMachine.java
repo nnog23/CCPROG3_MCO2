ebb9cba6f0482369bc27c65e08ee574604cca079
nnog23 <136414069+nnog23@users.noreply.github.com>
2023-08-03 23:29:19 +0800

    Final SpecialVendingMachine.java

--- SpecialVendingMachine.java
+++ SpecialVendingMachine.java
@@ -8,18 +8,14 @@ import java.util.Scanner;
 public class SpecialVendingMachine extends RegularVendingMachine{
 
 /**
- * 
+ * The constructor creates an instance for the special vending machine
  * 
  */
 public SpecialVendingMachine(){
 
   
 
-
 }
-
-
- 
 
 
 }
